dbd1d8a50f65a0870ad79abedb9b699688eeae5f
Tyler Long <aethdae@gmail.com>
2026-02-17 16:22:17 -0600

    initial commit: add boilerplate, add test.tsx

---
+++ src/Test.tsx
@@ -0,0 +1,21 @@
+import "./Test.css";
+
+function Test() {
+  return (
+    <>
+      <div className="wrapper">
+        <div className="divClass">
+          <h2>Test page</h2>
+        </div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus,
+          quasi consequuntur obcaecati necessitatibus eius nesciunt quidem modi,
+          doloremque ut fugit velit, aperiam beatae odio? Laudantium nostrum nam
+          delectus corporis consequuntur!
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default Test;
